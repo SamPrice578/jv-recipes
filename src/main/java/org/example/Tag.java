66101fa7920042128b378e11edeb83ec21aab1dc
Sam Price <samprice@Sams-MacBook-Pro.local>
2024-12-04 16:35:04 +0000

    feat: implement Tag Entity

---
+++ src/main/java/org/example/Tag.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Tag() {
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
