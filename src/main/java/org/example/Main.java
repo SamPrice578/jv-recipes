2f757dbf986fa009a9cba7a7f57a4f827756824e
Sam Price <samprice@Sams-MacBook-Pro.local>
2024-12-04 16:45:41 +0000

    update main to test insertion of entities into database

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,25 +9,8 @@ public class Main {
         SessionFactory sessionFactory = Database.getSessionFactory();
         sessionFactory.getSchemaManager().exportMappedObjects(true);
 
-//        Session session = sessionFactory.openSession();
-//        Transaction tx = null;
-//        try {
-//	        tx = session.beginTransaction();
-//	        Category category = new Category(1, "Cake");
-//            session.merge(category);
-//            Category cake = session.get(Category.class, 1);
-//            System.out.println(cake.toString());
-//            tx.commit();
-//        } catch (Exception e) {
-//            if(tx != null) { //or tx != null, Chris isn't sure of best practice`
-//                tx.rollback();
-//            }
-//        } finally {
-//	        session.close();
-//        }
-
         sessionFactory.inTransaction(session ->
-                session.persist(new Category(1, "Cake")));
+                session.persist(new Category("Cake")));
 
         sessionFactory.inTransaction(session -> {
             Category cake = session.get(Category.class, 1);
@@ -35,7 +18,7 @@ public class Main {
         });
 
         sessionFactory.inTransaction(session -> {
-            session.persist(new Ingredient(1, "Butter", 200, "g"));
+            session.persist(new Ingredient("Butter", 200, "g"));
         });
 
         sessionFactory.inTransaction(session -> {
@@ -44,7 +27,7 @@ public class Main {
         });
 
         sessionFactory.inTransaction(session -> {
-            session.persist(new RecipeUser(1, "Sam", "Sam@northcoders.com", "12345", UserRole.Admin, Date.valueOf("2024-01-01")));
+            session.persist(new RecipeUser("Sam", "Sam@northcoders.com", "12345", UserRole.Admin, Date.valueOf("2024-01-01")));
         });
 
         sessionFactory.inTransaction(session -> {
@@ -58,7 +41,7 @@ public class Main {
             Category category = session.get(Category.class, 1);
             Category[] categories = {category};
             RecipeUser recipeUser = session.get(RecipeUser.class, 1);
-            session.persist(new Recipe(1, "Chocolate Cake", "Tasty cake", "Mix ingredients and cook...", 15, 30, 5, 10, ingredients, categories, recipeUser, Date.valueOf("2024-01-01"), Date.valueOf("2024-02-01")));
+            session.persist(new Recipe("Chocolate Cake", "Tasty cake", "Mix ingredients and cook...", 15, 30, 1, 5, 10, ingredients, categories, recipeUser, Date.valueOf("2024-01-01"), Date.valueOf("2024-02-01")));
         });
 
         sessionFactory.inTransaction(session -> {
@@ -66,10 +49,48 @@ public class Main {
             System.out.println(recipe);
         });
 
+        sessionFactory.inTransaction(session -> {
+            Recipe recipe = session.get(Recipe.class, 1);
+            RecipeUser recipeUser = session.get(RecipeUser.class, 1);
+            session.persist(new Comment("This cake is great!", Date.valueOf("2024-02-03"), recipeUser, recipe));
+        });
 
+        sessionFactory.inTransaction(session -> {
+            Comment comment = session.get(Comment.class, 1);
+            System.out.println(comment);
+        });
 
+        sessionFactory.inTransaction((session -> {
+            session.persist(new Tag("Chocolatey"));
+        }));
 
+        sessionFactory.inTransaction(session -> {
+            Tag tag = session.get(Tag.class, 1);
+            System.out.println(tag);
+        });
 
+        sessionFactory.inTransaction((session -> {
+            String inputString = "cake pic";
+            byte[] byteArray = inputString.getBytes();
+            Recipe recipe = session.get(Recipe.class, 1);
+            session.persist(new Image("cake_picture", byteArray, recipe));
+        }));
+
+        sessionFactory.inTransaction(session -> {
+            Image image = session.get(Image.class, 1);
+            System.out.println(image);
+        });
+
+        sessionFactory.inTransaction((session -> {
+            Recipe recipe = session.get(Recipe.class, 1);
+            RecipeUser recipeUser = session.get(RecipeUser.class, 1);
+            session.persist(new Rating(5, Date.valueOf("2024-02-03"), recipe, recipeUser));
+        }));
+
+        sessionFactory.inTransaction(session -> {
+            Image image = session.get(Image.class, 1);
+            System.out.println(image);
+        });
 
     }
 }
